7681f3e973822b6241fc41d96b006872cd9f56ea
Ilia Sergachev <ilia.sergachev@protonmail.ch>
2018-04-03 15:34:03 +0200

    fix

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import shutil
 import pathlib
 import os.path
 # import pprint
+# import argparse
 
 API_ENDPOINT = 'https://cloud-api.yandex.net/v1/disk/public/resources/{}?public_key={}'
 
@@ -17,14 +18,15 @@ def save_file(url, save_path):
 def recurse(url, save_path):
     pathlib.Path(save_path).mkdir(parents=True, exist_ok=True)
     items = requests.get(API_ENDPOINT.format('', url)).json()['_embedded']['items']
-    ret = []
     for i in items:
+        # pprint.pprint(i)
         new_path = os.path.join(save_path, i['name'])
         if 'file' in i:
             print('file {}'.format(new_path))
             save_file(i['file'], new_path)
         else:
             print('folder {}'.format(new_path))
+            recurse(i['public_url'], new_path)
 
 
 target_root = 'https://yadi.sk/d/AhgSCPivmcMff'
