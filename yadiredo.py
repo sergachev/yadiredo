d593504fbcc415cd08c7b6d60d0b2f948e1f186e
Ilia Sergachev <ilia.sergachev@protonmail.ch>
2020-02-22 15:15:29 +0100

    refactor; add request throttling delay; print 'too many requests'; make checksum verification optional

--- yadiredo.py
+++ yadiredo.py
@@ -8,76 +8,104 @@ import argparse
 import hashlib
 import requests
 from logzero import logger as log
+from pprint import pformat
+from time import sleep
 
 
-API_ENDPOINT = 'https://cloud-api.yandex.net/v1/disk/public/resources/?public_key={}&path=/{}&offset={}'
-dry = False
+class Yadiredo:
+    API_ENDPOINT = 'https://cloud-api.yandex.net/v1/disk/public/resources/?public_key={}&path=/{}&offset={}'
 
+    def __init__(self, verify_only, verify_checksums, delay, *args, **kwargs):
+        self.verify_only = verify_only
+        self.verify_checksums = verify_checksums
+        self.delay = delay
 
-def md5sum(file_path):
-    md5 = hashlib.md5()
-    with open(file_path, 'rb') as f:
-        for chunk in iter(lambda: f.read(128 * md5.block_size), b''):
-            md5.update(chunk)
-    return md5.hexdigest()
+    def md5sum(self, file_path):
+        md5 = hashlib.md5()
+        with open(file_path, 'rb') as f:
+            for chunk in iter(lambda: f.read(128 * md5.block_size), b''):
+                md5.update(chunk)
+        return md5.hexdigest()
 
-
-def check_and_download_file(target_path, url, size, checksum):
-    if os.path.isfile(target_path):
-        if size == os.path.getsize(target_path):
-            if checksum == md5sum(target_path):
-                log.info('skipping correct {}'.format(target_path))
-                return
-    if not dry:
-        log.info('downloading {}'.format(target_path))
+    def download_file(self, target_path, url):
+        log.info('downloading')
         r = requests.get(url, stream=True)
         with open(target_path, 'wb') as f:
             shutil.copyfileobj(r.raw, f)
 
+    def check_local_file(self, target_path, size, checksum):
+        if os.path.isfile(target_path):
+            if size == os.path.getsize(target_path):
+                if checksum == self.md5sum(target_path):
+                    return True
+                else:
+                    log.warning('checksum mismatch')
+            else:
+                log.warning('size mismatch')
+        else:
+            log.warning('missing')
+        return False
 
-def download_path(target_path, public_key, source_path, offset=0):
-    log.info('getting "{}" at offset {}'.format(source_path, offset))
-    current_path = os.path.join(target_path, source_path)
-    pathlib.Path(current_path).mkdir(parents=True, exist_ok=True)
-    jsn = requests.get(API_ENDPOINT.format(public_key, source_path, offset)).json()
-
-    def try_as_file(j):
+    def try_as_file(self, j, current_path):
         if 'file' in j:
             file_save_path = os.path.join(current_path, j['name'])
-            check_and_download_file(file_save_path, j['file'], j['size'], j['md5'])
+            log.info(f'processing {file_save_path}')
+            if not self.check_local_file(file_save_path, j['size'], j['md5']):
+                if not self.verify_only:
+                    self.download_file(file_save_path, j['file'])
+            else:
+                log.info('correct')
             return True
         return False
 
-    # first try to treat the actual json as a single file description
-    if try_as_file(jsn):
-        return
+    def download_path(self, target_path, public_key, source_path, offset=0):
+        sleep(self.delay)
+        log.info('getting "{}" at offset {}'.format(source_path, offset))
+        current_path = os.path.join(target_path, source_path)
+        pathlib.Path(current_path).mkdir(parents=True, exist_ok=True)
+        jsn = requests.get(self.API_ENDPOINT.format(public_key, source_path, offset)).json()
 
-    # otherwise treat it as a directory
-    emb = jsn['_embedded']
-    items = emb['items']
-    for i in items:
-        # each item can be a file...
-        if try_as_file(i):
-            continue
-        # ... or a directory
-        else:
-            subdir_path = os.path.join(source_path, i['name'])
-            download_path(target_path, public_key, subdir_path)
+        # first try to treat the actual json as a single file description
+        if self.try_as_file(jsn, current_path):
+            return
 
-    # check if current directory has more items
-    last = offset + emb['limit']
-    if last < emb['total']:
-        download_path(target_path, public_key, source_path, last)
+        # otherwise treat it as a directory
+        try:
+            emb = jsn['_embedded']
+        except KeyError:
+            log.error(pformat(jsn))
+            return
+        items = emb['items']
+        for i in items:
+            # each item can be a file...
+            if self.try_as_file(i, current_path):
+                continue
+            # ... or a directory
+            else:
+                subdir_path = os.path.join(source_path, i['name'])
+                self.download_path(target_path, public_key, subdir_path)
+
+        # check if current directory has more items
+        last = offset + emb['limit']
+        if last < emb['total']:
+            self.download_path(target_path, public_key, source_path, last)
 
 
-parser = argparse.ArgumentParser(description='Yandex.Disk downloader.')
-parser.add_argument('url')
-parser.add_argument('-o', dest='output_path', default='output')
-parser.add_argument('--dry', action='store_const', const=True, default=False)
-parser.add_argument('-r', dest='retries', default=None)
-args = parser.parse_args()
-dry = args.dry
-if args.retries:
-    requests.adapters.DEFAULT_RETRIES = args.retries
+def main():
+    parser = argparse.ArgumentParser(description='Yandex.Disk downloader.')
+    parser.add_argument('url')
+    parser.add_argument('-o', dest='output_path', default='output')
+    parser.add_argument('--verify_only', action='store_const', const=True, default=False)
+    parser.add_argument('--verify_checksums', action='store_const', const=True, default=False)
+    parser.add_argument('--delay', type=int, action='store', default=1)
+    parser.add_argument('-r', dest='retries', default=None)
+    args = parser.parse_args()
+    if args.retries:
+        requests.adapters.DEFAULT_RETRIES = args.retries
 
-download_path(args.output_path, args.url, '')
+    d = Yadiredo(**vars(args))
+    d.download_path(args.output_path, args.url, '')
+
+
+if __name__ == '__main__':
+    main()
